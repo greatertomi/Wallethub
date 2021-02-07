3473fdf070cd1f9382a455f3283e6807af6e0874
Oshalusi John <oshalusijohn@gmail.com>
2021-02-07 21:08:50 +0100

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { environment } from '../environments/environment';
 import { Test1Component } from './test1/test1.component';
 import {FormsModule} from '@angular/forms';
 import {ChildComponent, Test02Component, TextField} from './test2/components';
-import { Playground1Component } from './playground1/playground1.component';
-import { Playground2Component } from './playground2/playground2.component';
 import { Test3Component } from './test3/test3.component';
 import { Test4Component } from './test4/test4.component';
 import { Test6Component } from './test6/test6.component';
@@ -22,8 +20,6 @@ import {NgxLinkifyjsModule} from 'ngx-linkifyjs';
     Test02Component,
     ChildComponent,
     TextField,
-    Playground1Component,
-    Playground2Component,
     Test3Component,
     Test4Component,
     Test6Component
